035fdd4f11a0e6522e02ff7fae641098d72b6be8
Vegar Westerlund <vegarwe@gmail.com>
2013-10-11 20:49:56 +0200

    Just add sample code for what needs to be handled by the num_complete module

---
+++ lib/num_complete.py
@@ -0,0 +1,8 @@
+
+# HciDisconnectionComplete(HciEventPkt):
+#         numComplete.available(0)
+# HciNumCompletePackets(HciEvent):
+#         numComplete.update(self.NumPackets)
+# HciLeConnectionComplete(HciEventPkt):
+#         numComplete.reset()
+
