0b6c8833c589fbbb4a5377934a96e05803f6b7e0
Vegar Westerlund <vegarwe@gmail.com>
2014-03-23 21:16:58 +0100

    Start reorganizing

--- num_complete.py
+++
@@ -1,8 +0,0 @@
-
-# HciDisconnectionComplete(HciEventPkt):
-#         numComplete.available(0)
-# HciNumCompletePackets(HciEvent):
-#         numComplete.update(self.NumPackets)
-# HciLeConnectionComplete(HciEventPkt):
-#         numComplete.reset()
-
